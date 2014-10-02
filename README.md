c84f4e67fe5924ffd7dd05bd8be59b651e2eb430
Jordan Rousseau <jrousseau@wdtinc.com>
2014-10-02 14:41:19 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+This is to test a prototype vector tile from wdt comparing it to a mapbox vector tile
+
+usage:
+
+`npm install;`
+`node index.js`
---
+++ index.js
@@ -0,0 +1,9 @@
+var VectorTile = require('vector-tile').VectorTile;
+var Pbf = require('pbf')
+var fs = require('fs');
+
+var mapbox_vt = new VectorTile(new Pbf(fs.readFileSync('./7.vector.pbf')));
+console.log(mapbox_vt);
+
+var wdt_vt = new VectorTile(new Pbf(fs.readFileSync('./L2_X1_Y1.pbf')));
+console.log(wdt_vt);
